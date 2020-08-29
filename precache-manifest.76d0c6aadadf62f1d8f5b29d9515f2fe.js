0164bf50d7d19401fc47af804dab6b95a4ef4ea0
Munish Thakur <munish81198@gmail.com>
2020-08-29 16:43:29 +0530

    Updates

---
+++ precache-manifest.76d0c6aadadf62f1d8f5b29d9515f2fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "728de31873efdc71ca69b047c4b6d6f1",
+    "url": "/typing-speed/index.html"
+  },
+  {
+    "revision": "2636f3d0055fd33b0d30",
+    "url": "/typing-speed/static/css/main.0f600b1e.chunk.css"
+  },
+  {
+    "revision": "4a5bc9bf26c6faa53844",
+    "url": "/typing-speed/static/js/2.7f3b59ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/typing-speed/static/js/2.7f3b59ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2636f3d0055fd33b0d30",
+    "url": "/typing-speed/static/js/main.84d2dc28.chunk.js"
+  },
+  {
+    "revision": "2dd4c053bb9713ea3c0e",
+    "url": "/typing-speed/static/js/runtime-main.bd22ac02.js"
+  }
+]);
